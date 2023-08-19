47418d90aacd604ca22baa059b00d623501a2574
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-08-19 12:57:11 +0300

    Подключила Firebase к проекту

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mobile8_final_project/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FoodShopApp());
 }
 
