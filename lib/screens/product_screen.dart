9836f20d1e9b78a147d03ffe8fef42b1398f068b
Svechka <34006663+Svechka@users.noreply.github.com>
2023-09-09 20:59:48 +0300

    categ&product

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile8_final_project/data/dto/product_dto.dart';
 import 'package:mobile8_final_project/screens/widgets/appbar.dart';
+import 'package:mobile8_final_project/mock_data.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen({super.key});
@@ -9,25 +11,54 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  ProductDto product = products[0];
+  bool isInCart = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const SafeArea(
+      child: SafeArea(
         child: Scaffold(
-          appBar: AppBarWidget(title: 'Название продукта',),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Экран продукта',
-                ),
-              ],
+            appBar: AppBarWidget(
+              title: product.name,
             ),
-          ),
-        ),
+            body: SingleChildScrollView(
+              child: Column(children: [
+                Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                      image:
+                          DecorationImage(image: NetworkImage(product.image))),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(product.description),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: isInCart
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.remove_circle_outline_sharp)),
+                            Text('1'),
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.add_circle_outline_sharp))
+                          ],
+                        )
+                      : ElevatedButton(
+                          onPressed: () {
+                            isInCart = true;
+                          },
+                          child: Text('В корзину'),
+                        ),
+                )
+              ]),
+            )),
       ),
     );
   }
--- lib/screens/products_in_category_screen.dart
+++ lib/screens/products_in_category_screen.dart
@@ -1,43 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:mobile8_final_project/data/dto/product_dto.dart';
 import 'package:mobile8_final_project/screens/product_screen.dart';
 import 'package:mobile8_final_project/screens/widgets/appbar.dart';
+
+import '../mock_data.dart';
 
 class ProductsInCategoryScreen extends StatefulWidget {
   const ProductsInCategoryScreen({super.key});
 
   @override
-  State<ProductsInCategoryScreen> createState() => _ProductsInCategoryScreenState();
+  State<ProductsInCategoryScreen> createState() =>
+      _ProductsInCategoryScreenState();
 }
 
 class _ProductsInCategoryScreenState extends State<ProductsInCategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    String categId = '1';
+    List<ProductDto> categProd = products
+        .where(
+          (element) => element.category == categId,
+        )
+        .toList();
     return Container(
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          appBar: const AppBarWidget(title: 'Название категории',),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Продукты в категории',
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ProductScreen()),
-                    );
-                  },
-                  child: const Text('Экран продукта'),
-                ),
-              ],
-            ),
+          appBar: AppBarWidget(
+            title: categories[int.parse(categId) - 1]['name'],
+          ),
+          body: SingleChildScrollView(
+            child: GridView.builder(
+                physics: ScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+                itemCount: categProd.length,
+                itemBuilder: (_, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ProductScreen()),
+                      );
+                    },
+                    child: SizedBox(
+                      height: 200,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 80,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.fitHeight,
+                                    image: NetworkImage(
+                                      categProd[index].image,
+                                    ))),
+                          ),
+                          Text(categProd[index].name),
+                          Text('${categProd[index].price} ₽')
+                        ],
+                      ),
+                    ),
+                  );
+                }),
           ),
         ),
       ),
